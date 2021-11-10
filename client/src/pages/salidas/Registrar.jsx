7cf7fc167c2183f3b98c0f4f6909943785344e05
Darealle <darealle@hotmail.com>
2021-11-09 20:52:33 -0600

    Se agrego el modulo Salidas al cliente con su respectiva configuracion en el serve

--- client/src/pages/salidas/Registrar.jsx
+++ client/src/pages/salidas/Registrar.jsx
@@ -1,40 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Form, Button, Row, Col, InputGroup } from "react-bootstrap";
-import SelectProductos from "./SelectProductos";
-import SelectProveedor from "./SelectProveedor";
 
-const Entradas = () => {
+const Salidas = () => {
 
-    // let getHoraActual = () => {
-    //     let today = new Date();
-    //     let horaActual = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-    //     return horaActual;
-    // }
-
-
-    // let getFechaActual = () => {
-    //     let today = new Date();
-    //     let fechaActual = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    //     return fechaActual;
-    // }
-
-    // console.log(getFechaActual());
-    // console.log(getHoraActual());
-   
     const [folio, setFolio] = useState("");
     const [sku, setSku] = useState("");
-    const [idProveedor, setIdProveedor] = useState("");
+    const [idCliente, setIdCliente] = useState("");
     const [fecha, setFecha] = useState("");
     const [hora, setHora] = useState("");
     const [cantidad, setCantidad] = useState("");
-    const [costoUnitario, setCostoUnitario] = useState("");
+    const [precioPublico, setPrecioPublico] = useState("");
+    const [descuento, setDescuento] = useState("");
 
 
     const onSubmitForm = async (e) => {
         try {
             e.preventDefault();
-            const body = { folio, sku, idProveedor, fecha, hora, cantidad, costoUnitario };
-            const response = await fetch("http://localhost:5000/entradas", {
+            const body = { folio, sku, idCliente, fecha, hora, cantidad, precioPublico, descuento };
+            const response = await fetch("http://localhost:5000/salidas", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(body)
@@ -63,32 +46,17 @@ const Entradas = () => {
 
                         <Form.Group as={Col} className="mb-3" controlId="formNumSku">
                             <Form.Label>SKU</Form.Label>
-                            <Form.Select aria-label="Default select example" value={sku} onChange={e => {setSku(e.target.value)}} >
-                            <option>Abrir para seleccionar</option>
-                            <option value="1">1</option>
-                            {/* {
-                                SelectProductos().map(producto => (
-                                    <option
-                                        key={producto.idproveedor}
-                                        value={proveedor.idproveedor}
-                                    >#{proveedor.idproveedor} | {proveedor.razonsocial} {console.log(proveedor)}</option>
-                                ))
-                            } */}
-                        </Form.Select>
+                            <Form.Select aria-label="Default select example" value={sku} onChange={e => { setSku(e.target.value) }} >
+                                <option>Abrir para seleccionar</option>
+                                <option value="1">1</option>
+                            </Form.Select>
                         </Form.Group>
                     </Row>
-                    <Form.Group className="mb-3" controlId="formIdProveedor">
-                        <Form.Label>ID Proveedor</Form.Label>
-                        <Form.Select aria-label="Default select example" value={idProveedor} onChange={e => {setIdProveedor(e.target.value)}} >
+                    <Form.Group className="mb-3" controlId="formIdCliente">
+                        <Form.Label>ID Cliente</Form.Label>
+                        <Form.Select aria-label="Default select example" value={idCliente} onChange={e => { setIdCliente(e.target.value) }} >
                             <option>Abrir para seleccionar</option>
-                            {
-                                SelectProveedor().map(proveedor => (
-                                    <option
-                                        key={proveedor.idproveedor}
-                                        value={proveedor.idproveedor}
-                                    >#{proveedor.idproveedor} | {proveedor.razonsocial} {console.log(proveedor)}</option>
-                                ))
-                            }
+                            <option value="1">1</option>
                         </Form.Select>
                     </Form.Group>
                     <Row className="align-items-center">
@@ -118,24 +86,35 @@ const Entradas = () => {
                             />
                         </Form.Group>
 
-                        <Form.Group as={Col} className="mb-3" controlId="formCostoUnitario">
-                            <Form.Label>Costo unitario</Form.Label>
+                        <Form.Group as={Col} className="mb-3" controlId="formPrecioPublico">
+                            <Form.Label>Precio Publico</Form.Label>
                             <InputGroup>
                                 <InputGroup.Text>$</InputGroup.Text>
                                 <Form.Control type="number" placeholder="0.01"
-                                    value={costoUnitario}
-                                    onChange={e => setCostoUnitario(e.target.value)}
+                                    value={precioPublico}
+                                    onChange={e => setPrecioPublico(e.target.value)}
                                 />
                             </InputGroup>
                         </Form.Group>
 
-                        <Form.Group as={Col} className="mb-3" controlId="formCostoTotal">
-                            <Form.Label>Costo Total</Form.Label>
+                        <Form.Group as={Col} className="mb-3" controlId="formDescuento">
+                            <Form.Label>Descuento</Form.Label>
+                            <InputGroup>
+                                <Form.Control type="number" placeholder="0.01"
+                                    value={descuento}
+                                    onChange={e => setDescuento(e.target.value)}
+                                />
+                                <InputGroup.Text>%</InputGroup.Text>
+                            </InputGroup>
+                        </Form.Group>
+
+                        <Form.Group as={Col} className="mb-3" controlId="formMontoTotal">
+                            <Form.Label>Monto Total</Form.Label>
                             <InputGroup>
                                 <InputGroup.Text>$</InputGroup.Text>
                                 <Form.Control type="number"
-                                    value={cantidad * costoUnitario}
-                                    onChange={() => { return cantidad * costoUnitario }}
+                                    value={(cantidad * precioPublico) - (((cantidad * precioPublico) * descuento) / 100)}
+                                    onChange={() => { return ((cantidad * precioPublico) - ((cantidad * precioPublico) * descuento) / 100)}}
                                 />
                             </InputGroup>
                         </Form.Group>
@@ -149,4 +128,4 @@ const Entradas = () => {
     )
 }
 
-export default Entradas;
+export default Salidas;
