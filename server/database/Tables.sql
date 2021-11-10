7e16f92ab68bdd84b053bd7b05b2ffe8ce4e19b3
Darealle <darealle@hotmail.com>
2021-11-09 19:17:03 -0600

    Se agrego configuracion del modulo entradas al serve

---
+++ server/database/Tables.sql
@@ -0,0 +1,84 @@
+create database cafeso;
+
+create table Usuario (
+    matricula smallint primary key,
+    password varchar(50) not null,
+    nombre varchar(50) not null,
+    apellidoPaterno varchar(50) not null,
+    apellidoMaterno varchar(50) not null
+); 
+
+--Catalogo proveedor:
+create table Proveedor (
+    idProveedor smallint serial primary key,
+    razonSocial varchar(100) not null,
+    rfc varchar(13) not null unique,
+    telefono varchar(10) not null,
+    correo varchar(50) not null,
+    direccion text
+);
+
+--Catalogo de clientes:
+create table Cliente (
+    idCliente serial primary key,
+    telefono varchar(10),
+    correo varchar(50),
+    nombre varchar(50) not null,
+    apellidoPaterno varchar(50) not null,
+    apellidoMaterno varchar(50) not null,
+    rfc varchar(13)
+);
+
+--Catalogo de producto:
+create table Producto (
+    sku smallint primary key,
+    descripcion varchar(50) not null,
+    precioUnitario numeric(10,2) not null check(precioUnitario >= 0),
+    iva numeric(3) not null check(iva >= 0 and iva <= 100) default 16,
+    precioPublico numeric(10,2),
+    stockSistema integer not null,
+    valorAlmacen numeric(10,2)
+);
+
+create table Inventario (
+    idInventario smallint primary key,
+    fecha date not null default current_date,
+	hora time not null default current_time(2),
+    sku smallint not null,
+    descripcion varchar(50) not null,
+    stockSistema integer not null,
+    stockFisico integer not null,
+    precioUnitario numeric(10,2) not null check(precioUnitario >= 0)
+);
+
+create table insumo(
+    sku smallint primary key,
+    descripcion varchar(50) not null,
+    costoUnitario numeric(10,2) not null check(costoUnitario >= 0),
+    unidadMedida varchar(50) not null,
+    stockSistema integer not null,
+    valorAlmacen numeric(10,2)
+);
+
+create table Entrada (
+    folio varchar primary key,
+    sku integer not null references insumo, 
+    idProveedor smallint references Proveedor,
+    fecha date not null default current_date,
+    hora time not null default current_time,
+    cantidad integer not null check(cantidad >= 0),
+    costoUnitario numeric(10,2) not null check(costoUnitario >= 0),
+    costoTotal numeric(10,2)
+);
+
+create table Salida (
+    folio varchar primary key,
+    sku integer not null references Producto,
+    idCliente smallint references Cliente,
+    fecha date not null default current_date,
+    hora time not null default current_time,
+    cantidad integer not null check(cantidad >= 0),
+    precioPublico numeric(10,2) not null check(precioPublico >= 0),
+    descuento numeric(3) not null check(descuento >= 0 and descuento <= 100),
+    montoTotal numeric(10,2)
+);
---
+++ server/database/Triggers.sql
@@ -0,0 +1,164 @@
+/* PRODUCTO */
+--  funcion  --
+CREATE OR REPLACE FUNCTION calculo_valoresProducto() RETURNS trigger AS
+'
+DECLARE
+precioIvaP decimal;
+valorAlmacenP decimal;
+
+BEGIN
+   IF pg_trigger_depth() <> 1 THEN
+      RETURN NEW;
+   END IF;
+   IF(NEW.sku IS NOT NULL) THEN
+
+      precioIvaP := (SELECT ((SELECT iva FROM producto WHERE sku = NEW.sku)/100)*preciounitario+preciounitario FROM producto WHERE sku = NEW.sku);
+      UPDATE producto SET preciopublico = precioIvaP WHERE sku = NEW.sku;
+
+      valorAlmacenP := (SELECT preciopublico*stocksistema FROM producto WHERE sku = NEW.sku);
+      UPDATE producto SET valoralmacen = valorAlmacenP WHERE sku = NEW.sku;
+      return NEW;
+      
+   END IF;
+END;
+' LANGUAGE plpgsql;
+--  trigger  --
+create trigger accion_valoresProducto after insert or update on producto 
+for each row execute procedure calculo_valoresProducto();
+
+
+/* INSUMOS */
+--  funcion  --
+CREATE OR REPLACE FUNCTION calculo_valoresInsumo() RETURNS trigger AS
+'
+DECLARE
+valorAlmacenI decimal;
+BEGIN
+   IF pg_trigger_depth() <> 1 THEN
+      RETURN NEW;
+   END IF;
+   IF(NEW.stocksistema IS NOT NULL) THEN
+      valorAlmacenI := (SELECT costounitario*stocksistema FROM insumo WHERE sku = NEW.sku);
+      UPDATE insumo SET valoralmacen = valorAlmacenI WHERE sku = NEW.sku;
+      return NEW;
+   END IF;
+END;
+' LANGUAGE plpgsql;
+-- trigger  --
+create trigger accion_valoresInsumo after insert or update on insumo  
+for each row execute procedure calculo_valoresInsumo();
+
+/* INVENTARIOS errrrrrrrrrrr*/
+--  funcion  --
+CREATE OR REPLACE FUNCTION calculo_inventario() RETURNS trigger AS
+'
+DECLARE
+skuProducto smallint;
+skuInsumo smallint;
+invProducto integer;
+invInsumo integer;
+valorAlmacenP decimal;
+valorAlmacenI decimal;
+
+BEGIN
+   skuProducto := (SELECT sku FROM producto WHERE sku = NEW.sku);
+   skuInsumo := (SELECT sku FROM insumo WHERE sku = NEW.sku);
+   
+   IF pg_trigger_depth() <> 1 THEN
+      RETURN NEW;
+   END IF;
+
+   IF(NEW.sku = skuProducto) THEN
+      invProducto := (SELECT stocksistema-stockfisico FROM inventario WHERE idinventario = NEW.idinventario);
+      UPDATE producto SET stocksistema = invProducto WHERE sku = NEW.sku;
+      valorAlmacenP := (SELECT preciopublico*stocksistema FROM producto WHERE sku = NEW.sku);
+      UPDATE producto SET valoralmacen = valorAlmacenP WHERE sku = NEW.sku;
+      return NEW;
+   END IF;
+
+   IF(NEW.sku = skuInsumo) THEN
+      invInsumo := (SELECT stocksistema-stockfisico FROM inventario WHERE idinventario = NEW.idinventario);
+      UPDATE insumo SET stocksistema = invInsumo WHERE sku = NEW.sku;
+      valorAlmacenI := (SELECT costounitario*stocksistema FROM insumo WHERE sku = NEW.sku);
+      UPDATE insumo SET valoralmacen = valorAlmacenI WHERE sku = NEW.sku;
+      return NEW;
+   END IF;
+END;
+' LANGUAGE plpgsql;
+--  trigger  --
+create or replace trigger actualizar_Inventario after insert on inventario 
+for each row execute procedure calculo_inventario();
+
+/* ENTRADA */
+--  funcion  --
+CREATE OR REPLACE FUNCTION calculo_costoTotalEntrada() RETURNS trigger AS
+'
+DECLARE
+costoTotalE decimal;
+entradaInsumo integer;
+cantidadInsumo integer;
+cantidadEntrada integer;
+totalInsumo integer;
+valorAlmacenI decimal;
+
+BEGIN
+   IF pg_trigger_depth() <> 1 THEN
+      RETURN NEW;
+   END IF;
+   IF(NEW.sku IS NOT NULL) THEN
+
+      costoTotalE := (SELECT cantidad*costounitario FROM entrada WHERE folio = NEW.folio);
+      UPDATE entrada SET costototal = costoTotalE WHERE sku = NEW.sku;
+
+      cantidadInsumo := (SELECT stocksistema FROM insumo WHERE sku = NEW.sku);
+      cantidadEntrada := (SELECT cantidad FROM entrada WHERE folio = NEW.folio);
+      totalInsumo := cantidadInsumo + cantidadEntrada;
+      UPDATE insumo SET stocksistema = totalInsumo WHERE sku = NEW.sku;
+
+      valorAlmacenI := (SELECT costounitario*stocksistema FROM insumo WHERE sku = NEW.sku);
+      UPDATE insumo SET valoralmacen = valorAlmacenI WHERE sku = NEW.sku;
+      return NEW;
+
+   END IF;
+END;
+' LANGUAGE plpgsql;
+--  trigger  --
+create or replace trigger actualizar_costoTotalEntrada after insert or update on entrada 
+for each row execute procedure calculo_costoTotalEntrada();
+
+
+/* SALIDA */
+--  funcion  --
+CREATE OR REPLACE FUNCTION calculo_costoTotalSalida() RETURNS trigger AS
+'
+DECLARE
+costoTotalS decimal;
+stockSistemaP integer;
+cantSalida integer;
+cantTotal integer;
+valorAlmacenS decimal;
+
+BEGIN
+   IF pg_trigger_depth() <> 1 THEN
+      RETURN NEW;
+   END IF;
+   IF(NEW.sku IS NOT NULL) THEN
+
+      costoTotalS := (SELECT (cantidad*preciopublico)-((cantidad*preciopublico)*(descuento/100)) FROM salida WHERE folio = NEW.folio);
+      UPDATE salida SET montototal = costoTotalS WHERE folio = NEW.folio;
+
+      stockSistemaP := (SELECT stocksistema FROM producto WHERE sku = NEW.sku);
+      cantSalida := (SELECT cantidad FROM salida WHERE folio = NEW.folio);
+      cantTotal := stockSistemaP - cantSalida;
+      UPDATE producto SET stocksistema = cantTotal WHERE sku = NEW.sku;
+
+      valorAlmacenS := (SELECT preciopublico*stocksistema FROM producto WHERE sku = NEW.sku);
+      UPDATE producto SET valoralmacen = valorAlmacenS WHERE sku = NEW.sku;
+      return NEW;
+
+   END IF;
+END;
+' LANGUAGE plpgsql;
+--  trigger  --
+create or replace trigger actualizar_costoTotalSalida after insert or update on salida 
+for each row execute procedure calculo_costoTotalSalida();
