c31efa22fcd40577b694aab8b6e7e2bf09eab960
JuanJBMs <JuanJBM@outlook.com>
2021-11-11 13:16:06 -0600

    Se agrego el modulo de Insumos al Cliente

---
+++ client/src/pages/insumos/Insumos.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Tabs, Tab } from "react-bootstrap";
+import RevisarYBuscar from "./RevisarYBuscar";
+import Registrar from "./Registrar";
+import "./insumos.scss";
+
+const Insumos = () => {
+
+  const [key, setKey] = useState('Registrar');
+
+  return (
+    <>
+      <div className="insumos">
+        <div className="container">
+          <div className="insumos__titulo">
+            <h2>Insumos</h2>
+          </div>
+          
+          <Tabs
+            id="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+            className="mb-2 w-100"
+            style={{ textDecoration: "none" }}
+          >
+            <Tab eventKey="Registrar" title="Registrar">
+              <Registrar />
+            </Tab>
+
+            <Tab eventKey="Revisar y buscar" title="Revisar y buscar">
+              <RevisarYBuscar />
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Insumos
