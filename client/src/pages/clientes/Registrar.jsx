5dc851a2f122da899f211a8641ddee0e37f766b6
JuanJBMs <JuanJBM@outlook.com>
2021-11-16 10:26:37 -0600

    Se quitaron variables que no se utilizaban en los modulos clientes y productos, que ocasionaban warnings en consola

--- client/src/pages/clientes/Registrar.jsx
+++ client/src/pages/clientes/Registrar.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Table, Tabs, Tab, Form, Button, Row, Col } from "react-bootstrap";
+import React, { useState } from 'react'
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 const Clientes = () => {
 
